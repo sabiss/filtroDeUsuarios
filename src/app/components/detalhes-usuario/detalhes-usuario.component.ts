865e20c052fbac83a702e30a93e7a1c50e5e4d71
sabiss <sabrinabezsilva@gmail.com>
2025-02-17 15:54:17 -0300

    FEAT: campo de detalhes do usuário selecionado

---
+++ src/app/components/detalhes-usuario/detalhes-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhes-usuario',
+  standalone: false,
+  templateUrl: './detalhes-usuario.component.html',
+  styleUrl: './detalhes-usuario.component.scss'
+})
+export class DetalhesUsuarioComponent {
+
+}
