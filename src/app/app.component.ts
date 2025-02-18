df0170db3aceae15539c28d312c6090be9516c45
sabiss <sabrinabezsilva@gmail.com>
2025-02-18 14:04:32 -0300

    FEAT: lógica de troc de dados entre app, lista de usuários e detalhes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { IUsuario } from './interfaces/usuario/usuario.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'filtroUsuarios';
+  UsuarioParaMostrarDetalhes: IUsuario = {} as IUsuario;
+
+  receberUsuario(usuario: IUsuario) {
+    console.log('executou');
+    this.UsuarioParaMostrarDetalhes = usuario;
+  }
 }
