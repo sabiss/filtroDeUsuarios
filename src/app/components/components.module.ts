499bedf46395a662114b022851e6f4c4eacb5645
sabiss <sabrinabezsilva@gmail.com>
2025-02-18 14:05:23 -0300

    FEAT: declarando os components usados

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { DetalhesUsuarioComponent } from './detalhes-usuario/detalhes-usuario.component';
+import { InputsComponent } from './inputs/inputs.component';
+import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DetalhesUsuarioComponent,
+    InputsComponent,
+    ListaUsuariosComponent,
+  ],
   imports: [CommonModule, AngularMaterialModule],
+  exports: [DetalhesUsuarioComponent, InputsComponent, ListaUsuariosComponent], //pra aparecer no AppComponent tem que estar exportado
 })
 export class ComponentsModule {}
