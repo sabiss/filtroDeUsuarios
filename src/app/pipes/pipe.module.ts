4048c83c2293eb30e9b667a8224e6ba30d0937b5
sabiss <sabrinabezsilva@gmail.com>
2025-02-18 16:14:26 -0300

    FEAT: add pipe status

--- src/app/pipes/pipe.module.ts
+++ src/app/pipes/pipe.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EnderecoPipe } from './endereco.pipe';
 import { TelefonePipe } from './telefone.pipe';
+import { StatusPipe } from './status.pipe';
 
 @NgModule({
-  declarations: [EnderecoPipe, TelefonePipe],
+  declarations: [EnderecoPipe, TelefonePipe, StatusPipe],
   imports: [CommonModule],
-  exports: [EnderecoPipe, TelefonePipe],
+  exports: [EnderecoPipe, TelefonePipe, StatusPipe],
 })
 export class PipeModule {}
