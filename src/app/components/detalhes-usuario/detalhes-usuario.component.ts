1dcd9f3aaa461f7e4c2d7a6c1d95d2cec0fe514e
sabiss <sabrinabezsilva@gmail.com>
2025-02-18 14:06:07 -0300

    FEAT: configurando input do usuário a ser exibido

--- src/app/components/detalhes-usuario/detalhes-usuario.component.ts
+++ src/app/components/detalhes-usuario/detalhes-usuario.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IUsuario } from '../../interfaces/usuario/usuario.interface';
 
 @Component({
   selector: 'app-detalhes-usuario',
   standalone: false,
   templateUrl: './detalhes-usuario.component.html',
-  styleUrl: './detalhes-usuario.component.scss'
+  styleUrl: './detalhes-usuario.component.scss',
 })
 export class DetalhesUsuarioComponent {
-
+  @Input({ required: true }) usuarioParaExibir: IUsuario = {} as IUsuario;
 }
