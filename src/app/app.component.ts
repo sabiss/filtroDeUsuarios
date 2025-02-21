1a9081e0eb77887cfdc4bab13c9fc91be83fb9e1
sabiss <sabrinabezsilva@gmail.com>
2025-02-21 14:36:22 -0300

    REFACTOR: externalizando lógica das funções que fazem a filtragem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IUsuario } from './interfaces/usuario/usuario.interface';
 import { ListaUsuarios } from './data/lista-usuarios';
 import { DadosDeFiltragem } from './interfaces/dados-de-filtragem.interface';
+import { filtrarListaUsuarios } from './utils/filtrar-lista-usuario';
 
 @Component({
   selector: 'app-root',
@@ -16,8 +17,6 @@ export class AppComponent {
   listaDeUsuarios: IUsuario[] = ListaUsuarios;
   listaUsuariosFiltrados: IUsuario[] = ListaUsuarios;
 
-  dadosParaFiltragem: DadosDeFiltragem = {} as DadosDeFiltragem;
-
   receberUsuarioParaDetalhes(usuario: IUsuario): void {
     this.UsuarioParaMostrarDetalhes = usuario;
     this.showDetalhesUsuario = true;
@@ -25,83 +24,9 @@ export class AppComponent {
 
   executarFiltragem(dados: DadosDeFiltragem): void {
     //função impura
-    this.listaUsuariosFiltrados = this.filtrarUsuarios(
+    this.listaUsuariosFiltrados = filtrarListaUsuarios(
       dados,
       this.listaDeUsuarios
     );
-  }
-  //FUNÇÕES PURAS
-  filtrarUsuarios(
-    dados: DadosDeFiltragem,
-    listaDeUsuarios: IUsuario[]
-  ): IUsuario[] {
-    const usuariosComMesmoNome = this.filtrarUsuarioPorNome(
-      dados.nome,
-      listaDeUsuarios
-    ); //retorna a lista de usuários original ou lista com mesmo nome
-
-    const usuariosComMesmoStatus = this.filtrarUsuarioPorStatus(
-      dados.status,
-      usuariosComMesmoNome
-    ); //retorna a lista com mesmo nome ou a lista com mesmo nome e status
-
-    const listaDeUsuariosFiltrados = this.filtrarPorData(
-      dados.dataInicio,
-      dados.dataFinal,
-      usuariosComMesmoStatus
-    ); //retorna lista com nome e status ou lista com nome, status e data
-
-    // é filtrado em 3 etapas para caso o user não digite todos os inputs e mesmo assim apareça algum usuario com alguma informação igual a um dos inputs
-    return listaDeUsuariosFiltrados;
-  }
-
-  filtrarUsuarioPorNome(
-    nomeDoUsuarioParaFiltrar: string,
-    listaUsuarios: IUsuario[]
-  ) {
-    if (!nomeDoUsuarioParaFiltrar) {
-      return listaUsuarios;
-    }
-
-    const listaDeUsuariosFiltrados = listaUsuarios.filter((usuario) =>
-      usuario.nome
-        .toLowerCase()
-        .includes(nomeDoUsuarioParaFiltrar.toLowerCase())
-    );
-
-    return listaDeUsuariosFiltrados;
-  }
-  filtrarUsuarioPorStatus(
-    statusParaFiltrar: boolean | undefined,
-    listaDeUsuarios: IUsuario[]
-  ) {
-    if (!statusParaFiltrar) {
-      return listaDeUsuarios;
-    }
-
-    const listaDeUsuariosFiltrados = listaDeUsuarios.filter(
-      (usuario) => usuario.ativo == statusParaFiltrar
-    );
-
-    return listaDeUsuariosFiltrados;
-  }
-  filtrarPorData(
-    dataInicialParaFiltrar: Date | undefined,
-    dataFinalParaFiltrar: Date | undefined,
-    listaDeUsuarios: IUsuario[]
-  ) {
-    if (!dataInicialParaFiltrar || !dataFinalParaFiltrar) {
-      return listaDeUsuarios;
-    }
-    const listaDeUsuariosFiltrados = listaDeUsuarios.filter((usuario) => {
-      const dataDoUsuario = new Date(usuario.dataCadastro);
-
-      const usuarioDentroDoIntervaloDaData =
-        dataDoUsuario >= dataInicialParaFiltrar &&
-        dataDoUsuario <= dataFinalParaFiltrar;
-
-      return usuarioDentroDoIntervaloDaData;
-    });
-    return listaDeUsuariosFiltrados;
   }
 }
---
+++ src/app/utils/filtrar-lista-usuario.ts
@@ -0,0 +1,78 @@
+import { DadosDeFiltragem } from '../interfaces/dados-de-filtragem.interface';
+import { IUsuario } from '../interfaces/usuario/usuario.interface';
+
+const filtrarPorData = (
+  dataInicialParaFiltrar: Date | undefined,
+  dataFinalParaFiltrar: Date | undefined,
+  listaDeUsuarios: IUsuario[]
+): IUsuario[] => {
+  const datasNaoSelecionadas =
+    dataInicialParaFiltrar == undefined || dataFinalParaFiltrar == undefined;
+  if (datasNaoSelecionadas) {
+    return listaDeUsuarios;
+  }
+  const listaDeUsuariosFiltrados = listaDeUsuarios.filter((usuario) => {
+    const dataDoUsuario = new Date(usuario.dataCadastro);
+
+    const usuarioDentroDoIntervaloDaData =
+      dataDoUsuario >= dataInicialParaFiltrar &&
+      dataDoUsuario <= dataFinalParaFiltrar;
+
+    return usuarioDentroDoIntervaloDaData;
+  });
+  return listaDeUsuariosFiltrados;
+};
+
+const filtrarUsuarioPorStatus = (
+  statusParaFiltrar: boolean | undefined,
+  listaDeUsuarios: IUsuario[]
+): IUsuario[] => {
+  if (!statusParaFiltrar) {
+    return listaDeUsuarios;
+  }
+
+  const listaDeUsuariosFiltrados = listaDeUsuarios.filter(
+    (usuario) => usuario.ativo == statusParaFiltrar
+  );
+
+  return listaDeUsuariosFiltrados;
+};
+
+const filtrarUsuarioPorNome = (
+  nomeDoUsuarioParaFiltrar: string,
+  listaUsuarios: IUsuario[]
+): IUsuario[] => {
+  if (!nomeDoUsuarioParaFiltrar) {
+    return listaUsuarios;
+  }
+
+  const listaDeUsuariosFiltrados = listaUsuarios.filter((usuario) =>
+    usuario.nome.toLowerCase().includes(nomeDoUsuarioParaFiltrar.toLowerCase())
+  );
+
+  return listaDeUsuariosFiltrados;
+};
+const filtrarListaUsuarios = (
+  dados: DadosDeFiltragem,
+  listaDeUsuarios: IUsuario[]
+): IUsuario[] => {
+  let listaDeUsuariosFiltrados: IUsuario[] = [];
+
+  listaDeUsuariosFiltrados = filtrarUsuarioPorNome(dados.nome, listaDeUsuarios); //retorna a lista de usuários original ou lista com mesmo nome
+
+  listaDeUsuariosFiltrados = filtrarUsuarioPorStatus(
+    dados.status,
+    listaDeUsuariosFiltrados
+  ); //retorna a lista com mesmo nome ou a lista com mesmo nome e status
+
+  listaDeUsuariosFiltrados = filtrarPorData(
+    dados.dataInicio,
+    dados.dataFinal,
+    listaDeUsuariosFiltrados
+  ); //retorna lista com nome e status ou lista com nome, status e data
+
+  // é filtrado em 3 etapas para caso o user não digite todos os inputs e mesmo assim apareça algum usuario com alguma informação igual a um dos inputs
+  return listaDeUsuariosFiltrados;
+};
+
+export { filtrarListaUsuarios };
