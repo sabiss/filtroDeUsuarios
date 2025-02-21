a121ff0687e4a6d09c4b7fde3a62d39ae7378362
sabiss <sabrinabezsilva@gmail.com>
2025-02-21 11:29:45 -0300

    FEAT: interface dos dados de filtragem

--- src/app/components/lista-usuarios/lista-usuarios.component.ts
+++ src/app/components/lista-usuarios/lista-usuarios.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IUsuario } from '../../interfaces/usuario/usuario.interface';
 import { ListaUsuarios } from '../../data/lista-usuarios';
+import { DadosDeFiltragem } from '../../interfaces/dados-de-filtragem.interface';
 
 @Component({
   selector: 'app-lista-usuarios',
@@ -11,6 +12,8 @@ import { ListaUsuarios } from '../../data/lista-usuarios';
 export class ListaUsuariosComponent {
   @Output() usuarioSelecionadoParaDetalhes = new EventEmitter<IUsuario>();
   @Input({ required: true }) listaUsuarios: IUsuario[] = [];
+  @Input() dadosDeUsuarioParaFiltragem: DadosDeFiltragem =
+    {} as DadosDeFiltragem;
 
   displayedColumns = ['nome', 'dataCadastro', 'status'];
 
---
+++ src/app/interfaces/dados-de-filtragem.interface.ts
@@ -0,0 +1,6 @@
+export interface DadosDeFiltragem {
+  nome: string;
+  dataInicio: Date | undefined;
+  dataFinal: Date | undefined;
+  status: boolean | undefined;
+}
